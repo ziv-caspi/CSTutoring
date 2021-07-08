2eb33ac5b48b20b0e114dd85e5e517e37eeeb0b7
ziv-caspi <caspifx@gmail.com>
2021-07-08 17:35:05 +0300

    Initial commit

---
+++ Runner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Strings.Exercise10("helloasdhelloAAAhello", "hello"));
+        }
+    }
+}
---
+++ Runner/Strings.cs
@@ -0,0 +1,27 @@
+﻿namespace Runner
+{
+    public class Strings
+    {
+        public static int Exercise10(string first, string second) {
+            int repeatsFound = 0;
+            int currentStringIndex = 0;
+            
+            for (int i = 0; i < first.Length; i++) {
+                if (first[i] == second[currentStringIndex]) {
+                    currentStringIndex++;
+                }
+                else {
+                    currentStringIndex = 0;
+                }
+			
+                if (currentStringIndex == second.Length) {
+                    repeatsFound++;
+                    currentStringIndex = 0;
+                }
+            }
+		
+		
+            return repeatsFound;
+        }
+    }
+}
